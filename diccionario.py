cdeb7ae30b85a255f0d8e6010faac1c0f7a2aa07
fraga <fragacsz@gmail.com>
2022-10-06 20:28:50 -0300

    Lista y diccionarios

---
+++ diccionario.py
@@ -0,0 +1,45 @@
+persona= {
+    "estatura": 1.74,
+    "cabello": "risado"
+
+}
+
+print(persona['estatura'])
+print(persona['cabello'])
+print('Este es mi diccionario = ', persona)
+
+#from optparse import Values
+
+
+#cantidadAlumnos = dict(matematica=20, lenguaje =10)
+#print(cantidadAlumnos)
+#print(cantidadAlumnos['matematica'])
+#cantidadAlumnos['matematica']=30
+#print(cantidadAlumnos)
+
+
+#print('ciencias' in cantidadAlumnos)
+#print('matematica' in cantidadAlumnos)
+#print(cantidadAlumnos.get('matematica'))
+
+#iterar dentro de un diccionario
+#for key, value in cantidadAlumnos.items():
+#    print(key)
+#    print(value)
+
+#Borra diccionario
+#cantidadAlumnos.clear()
+#print(cantidadAlumnos)
+
+
+#cantidadAlumnos = dict(matematica=20, lenguaje=13, historia=15, biologia=30)
+#suma = 0
+#for key, cantidad in cantidadAlumnos.items():
+#    print(key)
+#    print(cantidad)
+#    suma = suma + cantidad
+#print('El tolta es')
+#print(suma)
+
+
+    
---
+++ lista.py
@@ -0,0 +1,2 @@
+lista = [3,4,5,6,7,8,'Nombre']
+print('Esta es mi lista =', lista)
